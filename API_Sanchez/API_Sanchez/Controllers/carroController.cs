1bc4d6ca6c4961f8b7c6f5c7592263100bb68ee2
Stiven Sanchez <stsanchez92@gmail.com>
2020-01-13 20:31:29 -0600

    first Commit

---
+++ API_Sanchez/API_Sanchez/Controllers/carroController.cs
@@ -0,0 +1,43 @@
+﻿using API_Sanchez.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Sanchez.Controllers
+{
+    public class carroController : ApiController
+    {
+        Carro[] carros = new Carro[]
+        {
+            new Carro{idCarro=1,marca="Ferrari",modelo=2012},
+            new Carro{idCarro=2,marca="BMW",modelo=2016},
+            new Carro{idCarro=3,marca="Hyunday",modelo=2001},
+            new Carro{idCarro=4,marca="Honda",modelo=1992},
+            new Carro{idCarro=5,marca="Mercedez",modelo=2005},
+            new Carro{idCarro=6,marca="Nissan",modelo=2014}
+        };
+
+        public IEnumerable<Carro> getAllCarro()
+        {
+            return carros;
+        }
+
+
+        public IHttpActionResult getCarro(int id)
+        {
+            var carro = carros.FirstOrDefault(obj => obj.idCarro == id);
+            if (carro != null)
+            {
+                return Ok(carro);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ API_Sanchez/API_Sanchez/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_Sanchez.WebApiApplication" Language="C#" %>
